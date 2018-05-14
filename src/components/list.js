fffc9588d9166201d0f97860e9303cd8b04b5e77
Dylan Widjaja <dwidjaja945@gmail.com>
2018-05-14 14:52:24 -0700

    completed making the application display item list

---
+++ src/components/list.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import {getListData} from '../actions'
+
+class List extends Component {
+    componentDidMount() {
+        this.props.getListData();
+    };
+
+    render() {
+        const {listData} = this.props;
+
+        const listItems = listData.map( (item, itemIndex) => {
+            return (
+                <li className='collection-item' key={itemIndex}>{item.title}</li>
+            )
+        })
+        return(
+            <ul className='collection'>
+                {listItems}
+            </ul>
+        );
+    }
+}
+
+function mapStateToProps(state) {
+    return {
+        listData: state.list.all
+    }
+};
+
+export default connect( mapStateToProps, { getListData })(List);
